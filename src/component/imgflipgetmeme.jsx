5d98a572d30d81127d15e79038bebe4a327863f6
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-07-30 16:37:33 +0200

    i've display what i want and all of them at the same time no problemo

--- src/component/imgflipgetmeme.jsx
+++ src/component/imgflipgetmeme.jsx
@@ -1,26 +1,47 @@
-import { useEffect } from "react"
-import "../scss/style.css"
+import { useEffect, useState } from "react";
+import "../scss/style.css";
 import api from "./API_URL";
 
 function ImgFlipGetMeme() {
-    const [ImgFlipData, setImgFlipData] = useState(null);
+  const [imgFlipData, setImgFlipData] = useState([]);
 
-    useEffect(() => {
-        async function fectData() {
-            const response = await fetch(api);
-            const json = await response.json[0];
-            console.log(json)
-            setImgFlipData(json.results[0])
-            
-        }
-        fectData();
-    }, []);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(api);
+      const data = await response.json();
+      console.log(data);
+      setImgFlipData(data.data.memes);
+    }
+    fetchData();
+  }, []);
 
-    if (!ImgFlipData) {
-        return <span className="loader"></span>
-    };
+// on pouvais mettre aussi json à la place de data mais data semblent plus logique ducoup on laisse data
+//   useEffect(() => {
+//     async function fetchData() {
+//       const response = await fetch(api);
+//       const json = await response.json();
+//       console.log(json);
+//       setImgFlipData(json.data.memes);
+//     }
+//     fetchData();
+//   }, []);
 
-};
+  if (imgFlipData.length === 0) {
+    return <span className="loader"></span>;
+  }
 
+  return (
+    <div className="displayflex">
+    <article>
+      {imgFlipData.map((meme, index) => (
+        <div key={index}>
+          <img src={meme.url} />
+
+        </div>
+      ))}
+      </article>
+    </div>
+  );
+}
 
 export default ImgFlipGetMeme;
